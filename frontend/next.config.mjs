7f95059034b8b231910dedbd24fe50344730ce8e
Arnav1108 <arnavthakur0811@gmail.com>
2026-06-13 14:38:20 +0530

    fix: polyfill __dirname for ua-parser-js in edge runtime

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-};
+  webpack: (config, { isServer }) => {
+    config.node = {
+      ...config.node,
+      __dirname: true,
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
